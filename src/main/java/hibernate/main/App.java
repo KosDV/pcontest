f8ae5de8d212eab4aa46ec46de0d9ffba58c7986
El Taha, Nadim <nadimeltaha@gmail.com>
2014-05-12 21:03:06 +0200

    Adds urn implication into App.java

--- src/main/java/hibernate/main/App.java
+++ src/main/java/hibernate/main/App.java
@@ -2,6 +2,7 @@ package hibernate.main;
 
 import hibernate.generic.GenUserFunctions;
 import hibernate.model.Image;
+import hibernate.model.Urn;
 import hibernate.model.User;
 import hibernate.model.Vote;
 import hibernate.specific.UserFunctions;
@@ -18,11 +19,14 @@ import util.HibernateUtil;
 
 public class App {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		HibernateUtil.getSession().beginTransaction();
-
 		try {
+			/** URN **/
+			Urn urn = new Urn("Prueba");
+			HibernateUtil.getSession().save(urn);
+
 			/** USERS **/
 			User user1 = new User("Anna", "Andujar", "15/07/1990",
 					"ana@kaos.com", "1234", "477");
@@ -41,18 +45,23 @@ public class App {
 					user2);
 			Image img3 = new Image(103, 104, "Img03", "Imatge de prova 3",
 					user3);
-			
+
 			/** VOTES **/
 			Set<Image> images = new HashSet<Image>();
 			images.add(img1);
 			images.add(img2);
-			
+
 			Set<Image> images2 = new HashSet<Image>();
 			images2.add(img1);
 			images2.add(img3);
-			
+
 			Vote vote1 = new Vote(images);
+			vote1.setUrn(urn);
 			Vote vote2 = new Vote(images2);
+			vote2.setUrn(urn);
+
+			urn.getListVotes().add(vote1);
+			urn.getListVotes().add(vote2);
 
 			/** TRANSACTIONS **/
 			HibernateUtil.getSession().save(user1);
@@ -63,7 +72,7 @@ public class App {
 			HibernateUtil.getSession().save(img1);
 			HibernateUtil.getSession().save(img2);
 			HibernateUtil.getSession().save(img3);
-			
+
 			HibernateUtil.getSession().save(vote1);
 			HibernateUtil.getSession().save(vote2);
 
@@ -71,60 +80,66 @@ public class App {
 
 		} catch (Exception e) {
 			System.err.println("ERROR: There's been a problem.");
+			HibernateUtil.getSession().getTransaction().rollback();
+			throw e;
 		}
-
-		/*HibernateUtil.getSession().beginTransaction();
-
-		GenUserFunctions userF = new GenUserFunctions();
-		List<User> users = new ArrayList<User>();
-		List<Image> images = new ArrayList<Image>();
-
-		users = userF.findAll(User.class);
-		images = userF.findAll(Image.class);
-
-		HibernateUtil.getSession().getTransaction().commit();
-
-		System.out.println("======================");
-		System.out.println("LIST OF USERS & IMAGES");
-		System.out.println("======================");
-		for (int i = 0; i < users.size(); i++) {
-			User listUsers = users.get(i);
-			if (listUsers.getImage() != null)
-				System.out.println((i + 1) + "." + listUsers.getName() + " - "
-						+ listUsers.getImage().getTitle());
-		}
-
-		System.out.println("==============");
-		System.out.println("LIST OF IMAGES");
-		System.out.println("==============");
-		for (int i = 0; i < images.size(); i++) {
-			Image listImages = images.get(i);
-			System.out.println((i + 1) + "." + listImages.getTitle() + " - "
-					+ listImages.getUser().getName());
-		}
-
-		System.out.println("================");
-		System.out.println("FIND USER BY NIF");
-		System.out.println("================");
-
-		System.out.println("Type your NIF");
+		HibernateUtil.getSession().beginTransaction();
 		try {
-			BufferedReader bufferRead = new BufferedReader(
-					new InputStreamReader(System.in));
-			String input = bufferRead.readLine();
+			GenUserFunctions userF = new GenUserFunctions();
+			List<User> users = new ArrayList<User>();
+			List<Image> images = new ArrayList<Image>();
 
-			UserFunctions userF2 = new UserFunctions();
-			try {
-				User user = userF2.findByNif(input);
-				System.out.println("The user with NIF:" + input + " is "
-						+ user.getName() + " " + user.getSurname());
-			} catch (Exception e) {
-				System.err.println("NIF error");
-				e.printStackTrace();
+			users = userF.findAll(User.class);
+			images = userF.findAll(Image.class);
+
+			HibernateUtil.getSession().getTransaction().commit();
+			System.out.println("======================");
+			System.out.println("LIST OF USERS & IMAGES");
+			System.out.println("======================");
+			for (int i = 0; i < users.size(); i++) {
+				User listUsers = users.get(i);
+				if (listUsers.getImage() != null)
+					System.out.println((i + 1) + "." + listUsers.getName()
+							+ " - " + listUsers.getImage().getTitle());
 			}
-		} catch (IOException e) {
-			System.err.println("There's been an error.");
-		}*/
+
+			System.out.println("==============");
+			System.out.println("LIST OF IMAGES");
+			System.out.println("==============");
+			for (int i = 0; i < images.size(); i++) {
+				Image listImages = images.get(i);
+				System.out.println((i + 1) + "." + listImages.getTitle()
+						+ " - " + listImages.getUser().getName());
+			}
+
+			System.out.println("================");
+			System.out.println("FIND USER BY NIF");
+			System.out.println("================");
+
+			System.out.println("Type your NIF");
+			try {
+				BufferedReader bufferRead = new BufferedReader(
+						new InputStreamReader(System.in));
+				String input = bufferRead.readLine();
+
+				UserFunctions userF2 = new UserFunctions();
+
+				User user = userF2.findByNif(input);
+				if (user == null)
+					System.err.println("There is no user with the given NIF");
+				else
+					System.out.println("The user with NIF:" + input + " is "
+							+ user.getName() + " " + user.getSurname());
+
+			} catch (IOException e) {
+				System.err.println("There's been an error.");
+				throw e;
+			}
+		} catch (Exception e) {
+			System.err.println("ERROR: There's been a problem.");
+			HibernateUtil.getSession().getTransaction().rollback();
+			throw e;
+		}
 
 	}
 }
