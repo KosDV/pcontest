63a9062a18a5df0defa1012a5687daa172f21645
Ana AC <andujar.anna@gmail.com>
2014-05-18 13:11:45 +0200

    adds scripts

---
+++ src/main/webapp/js/homepage.js
@@ -0,0 +1,102 @@
+function Prova() {
+
+	var html = '<section><h2><span>Contact</span> us now</h2><p>If you are interested in either our project or our group, you can contact us on:</p><div><a href= "https://www.facebook.com/hemav.academics"><img src="caretos/fb.jpg" width="50" height="50" ></a><a href= "https://twitter.com/hemavgroup"><img src="caretos/twitter.jpg" width="50" height="50" ></a></div><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2717.434688773861!2d1.986894000000012!3d41.275161000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ses!2ses!4v1397242029465" width="600" height="450" frameborder="0" style="border:0"></iframe></section>';
+
+	document.getElementById("contingut-pagina").innerHTML = html;
+}
+
+function Home() {
+
+	var html = "";
+	html += "<section id=\"contingut\">";
+	html += "				<!--------------------- FIRST LINE --------------------->";
+	html += "				<div class=\"row\">";
+	html += "					<div class=\"col-md-4 portfolio-item\">";
+	html += "						<a href=\"#project-link\"> <img class=\"img-responsive\"";
+	html += "							src=\"\/assets\/sampleImg\/1.jpg\">";
+	html += "						<\/a>";
+	html += "						<h3>";
+	html += "							Sandland <input type=\"checkbox\" name=\"input\" value=\"Type here\">";
+	html += "						<\/h3>";
+	html += "					<\/div>";
+	html += "";
+	html += "					<div class=\"col-md-4 portfolio-item\">";
+	html += "						<a href=\"#project-link\"> <img class=\"img-responsive\"";
+	html += "							src=\"\/assets\/sampleImg\/4.jpg\">";
+	html += "						<\/a>";
+	html += "						<h3>";
+	html += "							Old Bambi <input type=\"checkbox\" name=\"input\" value=\"Type here\">";
+	html += "						<\/h3>";
+	html += "					<\/div>";
+	html += "";
+	html += "					<div class=\"col-md-4 portfolio-item\">";
+	html += "						<a href=\"#project-link\"> <img class=\"img-responsive\"";
+	html += "							src=\"\/assets\/sampleImg\/3.jpg\">";
+	html += "						<\/a>";
+	html += "						<h3>";
+	html += "							Skyrim <input type=\"checkbox\" name=\"input\" value=\"Type here\">";
+	html += "						<\/h3>";
+	html += "					<\/div>";
+	html += "				<\/div>";
+	html += "";
+	html += "				<!--------------------- SECOND LINE --------------------->";
+	html += "				<div class=\"row\">";
+	html += "					<div class=\"col-md-4 portfolio-item\">";
+	html += "						<a href=\"#project-link\"> <img class=\"img-responsive\"";
+	html += "							src=\"\/assets\/sampleImg\/5.jpg\">";
+	html += "						<\/a>";
+	html += "						<h3>";
+	html += "							NASCAR <input type=\"checkbox\" name=\"input\" value=\"Type here\">";
+	html += "						<\/h3>";
+	html += "					<\/div>";
+	html += "";
+	html += "					<div class=\"col-md-4 portfolio-item\">";
+	html += "						<a href=\"#project-link\"> <img class=\"img-responsive\"";
+	html += "							src=\"\/assets\/sampleImg\/6.jpg\">";
+	html += "						<\/a>";
+	html += "						<h3>";
+	html += "							Sunset <input type=\"checkbox\" name=\"input\" value=\"Type here\">";
+	html += "						<\/h3>";
+	html += "					<\/div>";
+	html += "";
+	html += "					<div class=\"col-md-4 portfolio-item\">";
+	html += "						<a href=\"#project-link\"> <img class=\"img-responsive\"";
+	html += "							src=\"\/assets\/sampleImg\/2.jpg\">";
+	html += "						<\/a>";
+	html += "						<h3>";
+	html += "							Station <input type=\"checkbox\" name=\"input\" value=\"Type here\">";
+	html += "						<\/h3>";
+	html += "					<\/div>";
+	html += "				<\/div>";
+	html += "";
+	html += "				<!--------------------- THIRD LINE --------------------->";
+	html += "				<div class=\"row\">";
+	html += "					<div class=\"col-md-4 portfolio-item\">";
+	html += "						<a href=\"#project-link\"> <img class=\"img-responsive\"";
+	html += "							src=\"http:\/\/placehold.it\/700x400\">";
+	html += "						<\/a>";
+	html += "						<h3>";
+	html += "							<input type=\"checkbox\" name=\"input\" value=\"Type here\">";
+	html += "						<\/h3>";
+	html += "					<\/div>";
+	html += "					<div class=\"col-md-4 portfolio-item\">";
+	html += "						<a href=\"#project-link\"> <img class=\"img-responsive\"";
+	html += "							src=\"http:\/\/placehold.it\/700x400\">";
+	html += "						<\/a>";
+	html += "						<h3>";
+	html += "							<input type=\"checkbox\" name=\"input\" value=\"Type here\">";
+	html += "						<\/h3>";
+	html += "					<\/div>";
+	html += "					<div class=\"col-md-4 portfolio-item\">";
+	html += "						<a href=\"#project-link\"> <img class=\"img-responsive\"";
+	html += "							src=\"http:\/\/placehold.it\/700x400\">";
+	html += "						<\/a>";
+	html += "						<h3>";
+	html += "							<input type=\"checkbox\" name=\"input\" value=\"Type here\">";
+	html += "						<\/h3>";
+	html += "					<\/div>";
+	html += "				<\/div>";
+	html += "			<\/section>";
+
+	document.getElementById("contingut-pagina").innerHTML = html;
+}
