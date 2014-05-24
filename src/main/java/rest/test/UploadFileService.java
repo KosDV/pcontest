42a5726e0766d4201c3893fee43d9fcf9f02991b
Ana AC <andujar.anna@gmail.com>
2014-05-24 16:22:51 +0200

    add UploadFie

--- src/main/java/rest/test/UploadFileService.java
+++ src/main/java/rest/test/UploadFileService.java
@@ -13,9 +13,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-import org.glassfish.jersey.media.multipart.FormDataParam;
-
 //import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 //import org.glassfish.jersey.media.multipart.FormDataParam;
 
