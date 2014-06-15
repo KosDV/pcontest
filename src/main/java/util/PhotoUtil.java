f56bb1f54e19bc04343debbeacb2d3f90a94caa1
El Taha, Nadim <nadimeltaha@gmail.com>
2014-06-15 03:01:03 +0200

    Adds a Singleton to contain a HashMap of photosToVote by Users

--- src/main/java/util/PhotoUtil.java
+++ src/main/java/util/PhotoUtil.java
@@ -1,13 +1,18 @@
 package util;
 
+import hibernate.model.Photo;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class PhotoUtil {
-    public static void writeToFile(InputStream photoS, File location) throws IOException {
+    public static void writeToFile(InputStream photoS, File location)
+	    throws IOException {
 	OutputStream out = new FileOutputStream(location, false);
 	int read = 0;
 	byte[] bytes = new byte[1024];
@@ -17,4 +22,25 @@ public class PhotoUtil {
 	out.flush();
 	out.close();
     }
+
+    public enum Singleton {
+
+	INSTANCE;
+
+	private HashMap<Integer, HashSet<Photo>> photosToVoteByUser = new HashMap<Integer, HashSet<Photo>>();
+
+	public void add(Integer userId, HashSet<Photo> photosToVote) {
+	    photosToVoteByUser.put(userId, photosToVote);
+	}
+
+	public HashSet<Photo> get(Integer userId) {
+	    return photosToVoteByUser.get(userId);
+	}
+
+	public void remove(Integer userId) {
+	    photosToVoteByUser.remove(userId);
+	}
+
+    }
+
 }
