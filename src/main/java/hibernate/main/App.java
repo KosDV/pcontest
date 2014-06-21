cf7dc2fde0b193165da3daf95e90b246459b0eeb
El Taha, Nadim <nadimeltaha@gmail.com>
2014-06-21 15:03:57 +0200

    Adds contest creation

--- src/main/java/hibernate/main/App.java
+++ src/main/java/hibernate/main/App.java
@@ -1,11 +1,15 @@
 package hibernate.main;
 
+import hibernate.manager.ContestManager;
 import hibernate.manager.UserManager;
+import hibernate.model.Contest;
 import hibernate.model.User;
 
 import java.util.Random;
 
+import util.DateUtil;
 import util.DigestUtil;
+import api.Status;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -25,5 +29,11 @@ public class App {
 
 	UserManager userManager = new UserManager();
 	userManager.saveNewUser(user);
+
+	Contest contest = new Contest(Status.PRESENTATIONS_OPENED,
+		DateUtil.getCurrentDate());
+
+	ContestManager contestMgr = new ContestManager();
+	contestMgr.saveNewContest(contest);
     }
 }
